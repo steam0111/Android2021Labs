676000e2bf6020b04601a08191c87337f3608e2f
Stanislav <steam0111@mail.ru>
2021-09-22 23:34:28 +0300

    project init
    Инициализация проекта

---
+++ app/src/main/java/com/share/art/MainActivity.kt
@@ -0,0 +1,29 @@
+package com.share.art
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.google.ar.core.codelab.cloudanchor.helpers.FullScreenHelper
+import com.share.art.ar.CloudAnchorFragment
+
+class MainActivity : AppCompatActivity(R.layout.main_activity) {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        addArFragment()
+    }
+
+    private fun addArFragment() {
+        var arFragment = supportFragmentManager.findFragmentById(R.id.arContainer)
+        if (arFragment == null) {
+            arFragment = CloudAnchorFragment()
+            supportFragmentManager
+                .beginTransaction()
+                .add(R.id.arContainer, arFragment).commit()
+        }
+    }
+
+    override fun onWindowFocusChanged(hasFocus: Boolean) {
+        super.onWindowFocusChanged(hasFocus)
+        FullScreenHelper.setFullScreenOnWindowFocusChanged(this, hasFocus)
+    }
+}
